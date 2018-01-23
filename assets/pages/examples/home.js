48cf851b44015cc7681d2bce952906fe9314e9a2
Patrick Stephan <pstephan1187@gmail.com>
2018-01-23 16:28:19 -0600

    Updated home page.

--- assets/pages/examples/home.js
+++ assets/pages/examples/home.js
@@ -7,6 +7,7 @@ module.exports = {
         <p>Vue.js Datatable is a drop-in component that displays tabular data in powerful ways while being easily configurable.</p>
         <p>The datatable component enables filtering and pagination (both of which can be controlled from anywhere on the page, along with sorting and display of more complicated data like buttons, edit screens, or just about anything.</p>
         <p>The Vue.js Datatable logic that controls the display of the data is also easily overriden so that you can control the sorting, filtering, or pagination manually. This allows you to use the datatable component for retrieving data via AJAX from any backend and URL schema.</p>
+        <p>While being very powerful and offering a lot of control over the underlying logic that powers the datatable, if you just need the default functionality, it is incredibly simple to use.</p>
       `,
     }
   ]
