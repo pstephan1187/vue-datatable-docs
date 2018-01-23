7e64274152fd18b9afc4665d9287fda6b425e333
Patrick Stephan <pstephan1187@gmail.com>
2018-01-23 17:07:29 -0600

    Added content to the quick start page.

--- assets/pages/examples/quick-start.js
+++ assets/pages/examples/quick-start.js
@@ -4,7 +4,10 @@ module.exports = {
     {
       id: 'quick-start',
       content: `
-<p>Use the component in your HTML or template. Then pass in the columns and the data to your Vue instance.</p>`,
+        <p>You will need to have Vue registered globally (by using the standard ES5 script tag for Vue or by assigning Vue to <code>window.Vue</code>) and have the datatable component registered (See <a href="/installation">Installation</a>).</p>
+
+        <p>Once you have Vue setup and the datatable component registered, you will need to drop the component where you want the table to display and assign your rows to the <code>data</code> attribute and your columns to the <code>columns</code> attribute (see the example).</p>
+      `,
       demo: {
         url: '/demonstrations/quick-start.html',
         height: '800px',
