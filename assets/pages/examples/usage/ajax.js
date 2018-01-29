97da6d6351742303aaf06089a9809d491d24d00a
Patrick Stephan <pstephan1187@gmail.com>
2018-01-28 23:11:33 -0600

    Made some updates to the AJAX demo.

--- assets/pages/examples/usage/ajax.js
+++ assets/pages/examples/usage/ajax.js
@@ -4,13 +4,21 @@ module.exports = {
       id: 'ajax',
       title: 'AJAX',
       content: `
-        <p>The datatable component comes pre-packaged with a pager component as well. The pager will automatically look for and discover any datatable components on the page and connect to them. This makes it super simple to add pagination anywhere on your page.</p>
+        <p>Vue.js Datatable allows you to pass a function to the <code>data</code> attribute as opposed to an array. This allows you to override the base functionality of the datatable to allow for more advanced or customized situations. This can include fetching data from an API via AJAX.</p>
 
-        <div class="alert alert-warning">
-          <p><strong>Note</strong>: the pager requires you to register a <code>v-model</code> attribute that points to a data parameter on your vue instance. This parameter must contain your page number. This would typically be <code>1</code> unless you wanted to datatable to load on a different page. See the example for clarification.</p>
-        </div>
+        <p>Below you will find an example where data is queried from the SpaceX database on previous launches.</p>
 
-        <p>The datatable component also supports filtering out of the box. Use the <code>filter-by</code> attribute to filter the data in the table. The filtering works in such a way that each row must contain at least one instance of every word in the filter. For example, if you filter by the text "george 10-11-1980", then any matching rows mush contain both "george" and "10-11-1980". Filtering is case-insensitive.</p>
+        <p>For any datatable where a function is passed to the <code>data</code> attribute, that function is called anytime there is a change to the filter, sorting, pagination, or column properties. 2 arguments are passed to the function everytime it is called. The first is an object that contains the necessary information to pull the rows needed:</p>
+
+        <ul>
+          <li><strong>filter</strong> - The value passed to the <code>filter-by</code> attribute.</li>
+          <li><strong>sort_by</strong> - The column that will be used for sorting.</li>
+          <li><strong>sort_dir</strong> - The direction by which you will sort the rows.</li>
+          <li><strong>page_length</strong> - The number of rows to show at once.</li>
+          <li><strong>page_number</strong> - The page number that you want to display.</li>
+        </ul>
+
+        <p>The second argument is a callback function that must be called to set the rows on the table. It takes 2 arguments. The first are the rows that you want displayed on the table, and the second is the total number of rows taking filtering (but not pagination) into effect.</p>
       `,
       demo: {
         url: '/demonstrations/usage/ajax.html',
